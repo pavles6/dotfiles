e11baad4ab05fb0895cf52a1bd5e9a160e6dd7e0
Pavle Sokic <pavles6@protonmail.com>
2025-02-10 11:00:30 +0100

    tweak neovim theme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      no_italic = true,
+      style = "mocha",
+      transparent_background = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
