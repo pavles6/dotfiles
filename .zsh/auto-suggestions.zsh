7c42f40c2de89d51b639e4e72decb966fa86ab92
pavles6 <pavles6@protonmail.com>
2025-08-28 18:52:07 +0200

    load zellij first

--- .zsh/auto-suggestions.zsh
+++ .zsh/auto-suggestions.zsh
@@ -1,9 +1,2 @@
-PLATFORM=$(uname)
-
-# load zsh autosuggestions
-if [[ $PLATFORM == "Darwin" ]]; then
-    ZSH_PLUGIN_PATH=$(brew --prefix)/share
-elif [[ $PLATFORM =~ ^Linux ]]; then
-    ZSH_PLUGIN_PATH="/usr/share/zsh/plugins"
-fi
+ZSH_PLUGIN_PATH="/usr/share/zsh/plugins"
 source $ZSH_PLUGIN_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh
