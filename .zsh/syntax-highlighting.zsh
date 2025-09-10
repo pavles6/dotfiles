4259f4010e5ed662f8bf75e8871a0fd7ae4ee46e
Pavle Sokic <pavles6@protonmail.com>
2025-09-10 22:15:33 +0200

    adapt zsh conf to macos specifics

--- .zsh/syntax-highlighting.zsh
+++ .zsh/syntax-highlighting.zsh
@@ -1,9 +1,2 @@
-PLATFORM=$(uname)
-
-# load zsh syntax highlighting
-if [[ $PLATFORM == "Darwin" ]]; then
-    ZSH_PLUGIN_PATH=$(brew --prefix)/share
-elif [[ $PLATFORM =~ ^Linux ]]; then
-    ZSH_PLUGIN_PATH="/usr/share/zsh/plugins"
-fi
+ZSH_PLUGIN_PATH=$(brew --prefix)/share
 source $ZSH_PLUGIN_PATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
