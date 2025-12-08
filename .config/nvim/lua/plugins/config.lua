dde2e2d3e02dc8f18753974d31ebd4613e8e8974
Pavle Sokic <pavles6@protonmail.com>
2025-12-08 01:21:46 +0100

    zed,nvim: tidy and turn on bracket colorizer

--- .config/nvim/lua/plugins/config.lua
+++ .config/nvim/lua/plugins/config.lua
@@ -5,50 +5,36 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  {
-    "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        preset = "enter",
-        ["<C-f>"] = { "select_and_accept", "accept" },
-      },
-    },
-  },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      pickers = {
-        defaults = {
-          preview = {
-            filesize_limit = 0.1,
-          },
+    {
+        "saghen/blink.cmp",
+        opts = {
+            keymap = {
+                preset = "enter",
+                ["<C-f>"] = { "select_and_accept", "accept" },
+            },
         },
-        find_files = {
-          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        opts = {
+            pickers = {
+                defaults = {
+                    preview = {
+                        filesize_limit = 0.1,
+                    },
+                },
+                find_files = {
+                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+            },
         },
-      },
     },
-  },
-
-  -- catppuccin config
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
-
-  -- set catppuccin as a LazyVim theme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin-mocha",
+        },
     },
-  },
 }
